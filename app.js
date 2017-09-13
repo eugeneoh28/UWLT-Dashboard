caa86a86cb1f32bb7b62857a8f3110de67d51ff6
eugeneoh <eo28@uw.edu>
2017-09-13 16:56:08 -0700

    added stuff

--- app.js
+++ app.js
@@ -9,26 +9,31 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname +"/public"));
 app.set("view engine", "ejs");
 
-global.username = "";
+global.username = ":";
 global.password = "";
 
 app.get("/", function(req, res) {
-  request('http://' + username + password + '@uw.service-now.com/api/now/table/task?sysparm_query=active%3Dtrue%5Estate%3D2%5EORstate%3D1%5Eassignment_group%3D1854c1a06f1ca100ab448bec5d3ee4ef%5Esys_class_name%3Du_simple_requests&sysparm_display_value=true&sysparm_fields=state%2Cnumber&sysparm_limit=10',
+  request('http://' + username + password + '@uw.service-now.com/api/now/table/task?sysparm_query=active%3Dtrue%5Eassignment_group%3D1854c1a06f1ca100ab448bec5d3ee4ef%5EORassignment_group%3D6c54c1a06f1ca100ab448bec5d3ee4f2%5Esys_class_name%3Du_simple_requests%5Estate%3D1%5EORstate%3D2&sysparm_fields=number%2Cstate%2Csys_updated_on%2Cdescription&sysparm_limit=100',
           function(error, response, body){
             if(error) {
               console.log("error");
             }
             resp = {};
             resp.lms = JSON.parse(body).result;
-            resp.ticketnum = resp.lms.length;
-            request('http://' + username + password + '@uw.service-now.com/api/now/table/task?sysparm_query=assignment_group%3D63d9b9e96ff9a50090ead2054b3ee4ff%5EORassignment_group%3Dbfb292e26f26110054aafd16ad3ee4e4%5Estate%3D1%5EORstate%3D2&sysparm_fields=state%2Cnumber%2Csys_updated_on%2Ccmdb_ci&sysparm_limit=10',
+            request('http://' + username + password + '@uw.service-now.com/api/now/table/task?sysparm_query=active%3Dtrue%5Eassignment_group%3D0cf2d2e26f26110054aafd16ad3ee49a%5EORassignment_group%3D63d9b9e96ff9a50090ead2054b3ee4ff%5Esys_class_name%3Du_simple_requests%5Estate%3D1%5EORstate%3D2&sysparm_fields=number%2Cstate%2Csys_updated_on%2Cdescription&sysparm_limit=100',
                       function(error, response, body){
                         if(error) {
                           console.log("error");
                         }
                       resp.multimedia = JSON.parse(body).result;
-                      resp.ticketnum += resp.multimedia.length;
-                      res.render("home", {body:resp});
+                      request('http://' + username + password + '@uw.service-now.com/api/now/table/task?sysparm_query=active%3Dtrue%5Eassignment_group%3D0cf2d2e26f26110054aafd16ad3ee49a%5EORassignment_group%3D63d9b9e96ff9a50090ead2054b3ee4ff%5Esys_class_name%3Du_simple_requests%5Estate%3D1%5EORstate%3D2&sysparm_fields=number%2Cstate%2Csys_updated_on%2Cdescription&sysparm_limit=100',
+                                function(error, response, body){
+                                  if(error) {
+                                    console.log("error");
+                                  }
+                                resp.pollev = JSON.parse(body).result;
+                                res.render("home", {body:resp});
+                      });
             });
   });
 });
@@ -36,16 +41,31 @@ app.get("/", function(req, res) {
 
 io.on('connection', function(socket){
   socket.on('pingticket', function(data){
-    request('http://'+username + password +'@uw.service-now.com/api/now/table/task?sysparm_query=active%3Dtrue%5Estate%3D2%5EORstate%3D1%5Eassignment_group%3D1854c1a06f1ca100ab448bec5d3ee4ef%5Esys_class_name%3Du_simple_requests&sysparm_display_value=true&sysparm_fields=state%2Cnumber&sysparm_limit=10',
+    request('http://' + username + password + '@uw.service-now.com/api/now/table/task?sysparm_query=active%3Dtrue%5Eassignment_group%3D1854c1a06f1ca100ab448bec5d3ee4ef%5EORassignment_group%3D6c54c1a06f1ca100ab448bec5d3ee4f2%5Esys_class_name%3Du_simple_requests%5Estate%3D1%5EORstate%3D2&sysparm_fields=number%2Cstate%2Csys_updated_on%2Cdescription&sysparm_limit=100',
             function(error, response, body){
               if(error) {
                 console.log("error");
               }
-              var parsedBody = JSON.parse(body);
-              io.emit('ticketRefresh', parsedBody.result);
-            });
-  })
-})
+              resp = {};
+              resp.lms = JSON.parse(body).result;
+              request('http://' + username + password + '@uw.service-now.com/api/now/table/task?sysparm_query=active%3Dtrue%5Eassignment_group%3D0cf2d2e26f26110054aafd16ad3ee49a%5EORassignment_group%3D63d9b9e96ff9a50090ead2054b3ee4ff%5Esys_class_name%3Du_simple_requests%5Estate%3D1%5EORstate%3D2&sysparm_fields=number%2Cstate%2Csys_updated_on%2Cdescription&sysparm_limit=100',
+                        function(error, response, body){
+                          if(error) {
+                            console.log("error");
+                          }
+                        resp.multimedia = JSON.parse(body).result;
+                        request('http://' + username + password + '@uw.service-now.com/api/now/table/task?sysparm_query=active%3Dtrue%5Eassignment_group%3D0cf2d2e26f26110054aafd16ad3ee49a%5EORassignment_group%3D63d9b9e96ff9a50090ead2054b3ee4ff%5Esys_class_name%3Du_simple_requests%5Estate%3D1%5EORstate%3D2&sysparm_fields=number%2Cstate%2Csys_updated_on%2Cdescription&sysparm_limit=100',
+                                  function(error, response, body){
+                                    if(error) {
+                                      console.log("error");
+                                    }
+                                  resp.pollev = JSON.parse(body).result;
+                                  io.emit('ticketRefresh', resp);
+                        });
+              });
+    });
+  });
+});
 
 
 http.listen(3000, function(){
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -6,11 +6,11 @@ $(document).ready(function(){
 function refreshTicket(){
   socket.emit('pingticket', 'RefreshTickets!');
 }
-socket.on('ticketRefresh', function(data){
-  $('.canvasRequest').empty();
-  data.forEach(function(req){
-    $('.canvasRequest').append(
-      "<tr><th>" + req.number + "</th>" +
-      "<th>"+ req.state +"</th></tr>");
-  });
-})
+// socket.on('ticketRefresh', function(data){
+//   $('.canvasRequest').empty();
+//   data.forEach(function(req){
+//     $('.canvasRequest').append(
+//       "<tr><th>" + req.number + "</th>" +
+//       "<th>"+ req.state +"</th></tr>");
+//   });
+// })
