d0c4965a4df137494f53de169f34ec49b95cd656
euegne <eo28@uw.edu>
2017-09-20 17:02:38 -0700

    hello

--- app.js
+++ app.js
@@ -14,11 +14,13 @@ global.username = "";
 global.password = "";
 global.reqs = ['@uw.service-now.com/api/now/table/task?sysparm_query=active%3Dtrue%5Esys_class_name%3Du_simple_requests%5Eassignment_group%3D1854c1a06f1ca100ab448bec5d3ee4ef%5EORassignment_group%3D6c54c1a06f1ca100ab448bec5d3ee4f2%5Estate%3D1%5EORstate%3D2&sysparm_fields=number%2Cstate%2Csys_updated_on%2Cshort_description&sysparm_limit=100',
                 '@uw.service-now.com/api/now/table/task?sysparm_query=active%3Dtrue%5Esys_class_name%3Du_simple_requests%5Eassignment_group%3D0cf2d2e26f26110054aafd16ad3ee49a%5EORassignment_group%3D63d9b9e96ff9a50090ead2054b3ee4ff%5EORassignment_group%3Dbfb292e26f26110054aafd16ad3ee4e4%5Estate%3D1%5EORstate%3D2&sysparm_fields=number%2Cstate%2Csys_updated_on%2Cshort_description&sysparm_limit=100',
-                '@uw.service-now.com/api/now/table/task?sysparm_query=active%3Dtrue%5Esys_class_name%3Du_simple_requests%5Eassignment_group%3D6bbb84d16ff5650090ead2054b3ee414%5Estate%3D1%5EORstate%3D2&sysparm_fields=number%2Cstate%2Csys_updated_on%2Cshort_description&sysparm_limit=100']
-global.name = ['Canvas & Catalyst Requests', 'Multimedia Requests', 'Poll Everywhere Tickets']
+                '@uw.service-now.com/api/now/table/task?sysparm_query=active%3Dtrue%5Esys_class_name%3Du_simple_requests%5Eassignment_group%3D6bbb84d16ff5650090ead2054b3ee414%5Estate%3D1%5EORstate%3D2&sysparm_fields=number%2Cstate%2Csys_updated_on%2Cshort_description&sysparm_limit=100',
+                '@uw.service-now.com/api/now/table/task?sysparm_query=assignment_group%3D1854c1a06f1ca100ab448bec5d3ee4ef%5EORassignment_group%3D6c54c1a06f1ca100ab448bec5d3ee4f2%5EstateIN1%2C2%5Esys_class_name%3Dincident&sysparm_fields=number%2Csys_updated_on%2Cshort_description%2Cstate&sysparm_limit=100',
+                '@uw.service-now.com/api/now/table/task?sysparm_query=assignment_group%3D0cf2d2e26f26110054aafd16ad3ee49a%5EstateIN1%2C2%5Esys_class_name%3Dincident&sysparm_fields=number%2Csys_updated_on%2Cshort_description%2Cstate&sysparm_limit=100']
+global.consult = '@uw.service-now.com/api/now/table/task?sysparm_query=stateIN1%2C2%2C3%5Eu_template%3Da98cf3946f87ea00bd4906267b3ee470&sysparm_fields=number%2Cassigned_to%2Cdue_date&sysparm_limit=10'
+global.name = ['Canvas & Catalyst Requests', 'Multimedia Requests', 'Poll Everywhere Tickets', 'Canvas & Catalyst Incidents', 'Panopto Incidents']
 
 global.oldest = {};
-global.oldLongest = 0;
 global.asynchTasks = [];
 
 reqs.forEach(function(req) {
@@ -36,51 +38,32 @@ reqs.forEach(function(req) {
               resp.oldest = temp[j];
               longest = diff;
             }
-            if (diff > oldLongest) {
-              oldest = temp[j];
-              oldLongest = diff;
-            }
-            if (j == temp.length-1) {
-              callback(null, resp);
-            }
           }
+          callback(null, resp);
       });
   });
 });
 
+asynchTasks.push (function (callback) {
+  request('http://' + username + password + consult, function(error, response, body) {
+    if(error) {console.log(error);}
+    temp = JSON.parse(body).result;
+    callback(null, temp);
+  });
+});
+
 app.get("/", function(req, res) {
-  async.series(asynchTasks, function (err, results) {
-    res.render("home", {body:results, oldest:oldest, name:name});
+  async.parallel(asynchTasks, function (err, results) {
+    res.render("home", {body:results, name:name});
   })
 });
 
 
 io.on('connection', function(socket){
   socket.on('pingticket', function(data){
-    request('http://' + username + password + '@uw.service-now.com/api/now/table/task?sysparm_query=assignment_group%3D6bbb84d16ff5650090ead2054b3ee414%5Estate%3D1%5EORstate%3D2%5Esys_class_name%3Du_simple_requests&sysparm_fields=number%2Cstate%2Cdescription%2Csys_updated_on&sysparm_limit=100',
-            function(error, response, body){
-              if(error) {
-                console.log("error");
-              }
-              resp = {};
-              resp.lms = JSON.parse(body).result;
-              request('http://' + username + password + '@uw.service-now.com/api/now/table/task?sysparm_query=active%3Dtrue%5Eassignment_group%3D0cf2d2e26f26110054aafd16ad3ee49a%5EORassignment_group%3D63d9b9e96ff9a50090ead2054b3ee4ff%5Esys_class_name%3Du_simple_requests%5Estate%3D1%5EORstate%3D2&sysparm_fields=number%2Cstate%2Csys_updated_on%2Cdescription&sysparm_limit=100',
-                        function(error, response, body){
-                          if(error) {
-                            console.log("error");
-                          }
-                        resp.multimedia = JSON.parse(body).result;
-                        request('http://' + username + password + '@uw.service-now.com/api/now/table/task?sysparm_query=active%3Dtrue%5Eassignment_group%3D0cf2d2e26f26110054aafd16ad3ee49a%5EORassignment_group%3D63d9b9e96ff9a50090ead2054b3ee4ff%5Esys_class_name%3Du_simple_requests%5Estate%3D1%5EORstate%3D2&sysparm_fields=number%2Cstate%2Csys_updated_on%2Cdescription&sysparm_limit=100',
-                                  function(error, response, body){
-                                    if(error) {
-                                      console.log("error");
-                                    }
-                                  resp.pollev = JSON.parse(body).result;
-                                  io.emit('ticketRefresh', resp);
-                        });
-              });
-    });
-
+    async.parallel(asynchTasks, function (err, results) {
+      io.emit('ticketRefresh', {body:results, oldest:oldest, name:name});
+    })
   });
 });
 
